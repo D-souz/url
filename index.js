27f3fc3dba6c7ad03c9fc9f26149adeec9fb3afd
dsouza <dhesouzanorbert36@gmail.com>
2023-07-27 01:21:21 +0300

    Set up the files

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/', function(req, res) {
 
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+  res.json({ greeting: 'hello world' });
 });
 
 app.listen(port, function() {
